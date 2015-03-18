97124aae1d80d6667c99969d7b8e76b10662a739
Khaninz <khaninz3779@gmail.com>
2015-03-18 07:50:39 +0700

    - added more detail to profile page

--- app/src/main/java/soshop/social/soshop/ProfileActivity.java
+++ app/src/main/java/soshop/social/soshop/ProfileActivity.java
@@ -10,10 +10,12 @@ import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import soshop.social.soshop.Utils.ParseConstants;
@@ -27,16 +29,23 @@ public class ProfileActivity extends ActionBarActivity {
         setContentView(R.layout.activity_profile);
 
         final TextView mFriendsNumber;
+        final TextView mPostNumber;
+        final TextView mVoteNumber;
+        final TextView mProfileName;
+
 
         mFriendsNumber = (TextView) findViewById(R.id.friendsNumber);
+        mPostNumber = (TextView) findViewById(R.id.postNumber);
+        mVoteNumber = (TextView) findViewById(R.id.voteNumber);
+        mProfileName = (TextView) findViewById(R.id.profileName);
 
         Intent intent = getIntent();
         String senderId = intent.getStringExtra("SENDER_ID");
         String senderName = intent.getStringExtra("SENDER_FIRST_NAME");
 
-        Toast.makeText(this,senderName+ ": " +senderId,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, senderName + ": " + senderId, Toast.LENGTH_LONG).show();
 
-        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        final ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.whereEqualTo(ParseConstants.KEY_OBJECT_ID, senderId);
 
         query.findInBackground(new FindCallback<ParseUser>() {
@@ -45,19 +54,59 @@ public class ProfileActivity extends ActionBarActivity {
                 if (e == null) {
 
                     ParseUser displayUser = users.get(0);
+                    String displayUserId = displayUser.getObjectId();
+
+                    String firstName = displayUser.getString(ParseConstants.KEY_FIRST_NAME);
+                    String lastName = displayUser.getString(ParseConstants.KEY_LAST_NAME);
+
+                    mProfileName.setText(firstName+" "+lastName);
 
                     ParseRelation<ParseUser> friendsRelation = displayUser.getRelation(ParseConstants.KEY_RELATION_FRIENDS);
+                    friendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+                        @Override
+                        public void done(List<ParseUser> friends, ParseException e) {
 
-                        friendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
-                            @Override
-                            public void done(List<ParseUser> friends, ParseException e) {
+                            mFriendsNumber.setText(friends.size() + "");
+                        }
+                    });
 
-                                mFriendsNumber.setText(friends.size()+"");
-                            }
-                        });
+                    ParseQuery<ParseObject> queryForUserPost = ParseQuery.getQuery(ParseConstants.CLASS_SOSHOPPOST);
+                    queryForUserPost.whereEqualTo(ParseConstants.KEY_POST_SENDER_ID,displayUserId);
+                    queryForUserPost.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
+                    queryForUserPost.findInBackground(new FindCallback<ParseObject>() {
+                        @Override
+                        public void done(List<ParseObject> userPosts, ParseException e) {
+                            if(e==null){
+                            mPostNumber.setText(userPosts.size()+"");
+                        }}
+                    });
+
+                    ParseQuery<ParseObject> queryForUserSoShopVote = ParseQuery.getQuery(ParseConstants.CLASS_SOSHOPPOST);
+                    queryForUserSoShopVote.whereEqualTo(ParseConstants.KEY_IS_VOTE_SOSHOP_RELATION, displayUser);
+
+                    ParseQuery<ParseObject> queryForUserNoShopVote = ParseQuery.getQuery(ParseConstants.CLASS_SOSHOPPOST);
+                    queryForUserNoShopVote.whereEqualTo(ParseConstants.KEY_IS_VOTE_NOSHOP_RELATION, displayUser);
+
+                    List<ParseQuery<ParseObject>> queries = new ArrayList<ParseQuery<ParseObject>>();
+                    queries.add(queryForUserSoShopVote);
+                    queries.add(queryForUserNoShopVote);
+
+                    ParseQuery<ParseObject> queryForAllUserVote = ParseQuery.or(queries);
+
+                    // arrange to newest post at the top
+                    queryForAllUserVote.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
+
+                    queryForAllUserVote.findInBackground(new FindCallback<ParseObject>() {
+                        @Override
+                        public void done(List<ParseObject> parseObjects, ParseException e) {
+                            mVoteNumber.setText(parseObjects.size()+"");
+                        }
+                    });
+
+
 
                 } else {
-                    Toast.makeText(ProfileActivity.this, "error loading profile page: "+ e.toString(),Toast.LENGTH_LONG).show();
+                    Toast.makeText(ProfileActivity.this, "error loading profile page: " + e.toString(), Toast.LENGTH_LONG).show();
                 }
             }
         });
